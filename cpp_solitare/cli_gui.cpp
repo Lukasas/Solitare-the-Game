d2bfd33387ed5b357db9af1f61734729bac47ce7
David Benes <benys94@gmail.com>
2017-05-07 23:20:03 +0200

    - Some stuff moved into header file

--- cpp_solitare/cli_gui.cpp
+++ cpp_solitare/cli_gui.cpp
@@ -14,7 +14,8 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define KEY_SPACE 32
+#include "game_board.h"
+#include "cli_gui.h"
 
 void keyboard_interrupt(int signum)
 {
@@ -33,48 +34,6 @@ void resizeterm(int signum)
     wrefresh(stdscr);
     return;
 }
-
-typedef struct card_details{
-    std::string value;
-    bool turned;
-} CARD;
-
-typedef std::queue<CARD> CSTACK;
-
-typedef struct placeholder_details{
-    WINDOW *CARD_WINDOW;
-    int window_height;
-    int window_width;
-    bool blank;
-    bool secret;
-} PLACEHOLDER;
-
-typedef std::queue<PLACEHOLDER> CARD_SPOT;
-typedef std::map<int, CARD_SPOT> PLAYGROUND;
-
-class CLI{
-    public:
-        // Variables
-        int card_edge_size;
-        int y_gap, x_gap;
-
-        // Methods
-        CLI(); // Constructor
-        ~CLI(); // Destructor
-        void put_card_values(PLACEHOLDER window);
-        void arrow_control();
-
-    private:
-        // Variables
-        PLAYGROUND game_area;
-        int arrow_pos[2];
-
-        // Methods
-        void init_playground();
-        WINDOW * create_card_window(int start_y, int start_x);
-        void card_back(PLACEHOLDER window);
-        void draw_arrow(bool erase);
-};
 
 /**
  * @brief Constructor for CLI class
