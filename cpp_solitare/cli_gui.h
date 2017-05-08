4bfd614da5ea35f08fbc56d53265dbcd49cf940f
David Benes <benys94@gmail.com>
2017-05-08 14:35:33 +0200

    * Some cahnges in structure

--- cpp_solitare/cli_gui.h
+++ cpp_solitare/cli_gui.h
@@ -2,6 +2,7 @@
 #define _CLI_GUI_H_
 
 #include <string>
+#include <vector>
 #include <queue>
 #include <map>
 
@@ -11,23 +12,15 @@
 
 #define KEY_SPACE 32
 
-typedef struct card_details{
-    std::string value;
-    bool turned;
-} CARD;
-
-typedef std::queue<CARD> CSTACK;
-
 typedef struct placeholder_details{
     WINDOW *CARD_WINDOW;
-    int window_height;
-    int window_width;
-    bool blank;
-    bool secret;
+    std::vector<Card*> CDList;
+    int height;
+    int width;
+    bool expanded;
 } CARD_PLACEHOLDER;
 
-typedef std::queue<CARD_PLACEHOLDER> CARD_SPOT;
-typedef std::map<int, CARD_SPOT> PLAYGROUND;
+typedef std::map<int, CARD_PLACEHOLDER> PLAYGROUND;
 
 class CLI{
     public:
@@ -43,12 +36,15 @@ class CLI{
 
     private:
         // Variables
+        cGameBoard game_play;
         PLAYGROUND game_area;
         int arrow_pos[2];
 
         // Methods
         void init_playground();
         WINDOW * create_card_window(int start_y, int start_x);
+        std::string get_card_value(Card details);
+        void update_screen();
         void card_back(CARD_PLACEHOLDER window);
         void draw_arrow(bool erase);
 };
